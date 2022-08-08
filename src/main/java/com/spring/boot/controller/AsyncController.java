c805323be0b07b38f7398f8f992daf2cf65b5e14
name <email>
2022-08-08 16:54:34 +0800

    change

--- src/main/java/com/spring/boot/controller/AsyncController.java
+++ src/main/java/com/spring/boot/controller/AsyncController.java
@@ -1,8 +1,12 @@
 package com.spring.boot.controller;
 
 import com.spring.boot.service.AsyncService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.Future;
 
 /**
  * Controller层
@@ -15,14 +19,54 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/homepage")
 public class AsyncController {
 
+    private final static Logger logger = LoggerFactory.getLogger(com.spring.boot.controller.AsyncController.class);
+
+
     @Autowired
     AsyncService asyncTaskService;
 
+    /**
+     * 自定义实现线程异步
+     */
     @GetMapping("/learnAsync")
     public String learnAsync(){
-        for (int i = 0; i < 5; i++) {
-            asyncTaskService.executeAsyncTask(i);
+        for (int i = 0; i < 10; i++) {
+            try {
+                asyncTaskService.executeAsyncTask(i);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
         return "1";
+    }
+
+    @GetMapping("/learnFuture")
+    @SuppressWarnings("static-access")
+    public String index() {
+        long start = System.currentTimeMillis();
+        try {
+            logger.info("--------------------------------------------\n");
+            logger.info("每个任务执行的时间是：" + asyncTaskService.DoTime + "（毫秒）");
+
+            Future<Long> task = asyncTaskService.subByAsync();
+            asyncTaskService.subByVoid();
+            long sync = asyncTaskService.subBySync();
+
+            while (true) {
+                if (task.isDone()) {
+                    long async = task.get();
+                    logger.info("异步任务执行的时间是：" + async + "（毫秒）");
+                    logger.info("注解任务执行的时间是： -- （毫秒）");
+                    logger.info("同步任务执行的时间是：" + sync + "（毫秒）");
+                    break;
+                }
+            }
+            logger.info("--------------------------------------------\n");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        long end = System.currentTimeMillis();
+        logger.info("\t........请求响应时间为：" + (end - start) + "（毫秒）");
+        return "2";
     }
 }
--- src/main/java/com/spring/boot/service/AsyncService.java
+++ src/main/java/com/spring/boot/service/AsyncService.java
@@ -3,7 +3,10 @@ package com.spring.boot.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.Future;
 
 
 /**
@@ -16,10 +19,49 @@ import org.springframework.stereotype.Service;
 @Service
 public class AsyncService {
     private final static Logger logger = LoggerFactory.getLogger(com.spring.boot.service.AsyncService.class);
+    public static final int DoTime = 5000;
 
     @Async  // 表明该方法是异步方法。如果注解在类上，那表明类里面的所有方法都是异步
-    public void executeAsyncTask(int i) {
+    public void executeAsyncTask(int i) throws Exception {
         logger.info("\t 完成任务" + i);
         System.out.println("线程" + Thread.currentThread().getName() + " 执行异步任务：" + i);
+        Thread.sleep(10000);
     }
+
+
+    // 通过Future<T>来接受异步方法的处理结果
+    @Async
+    public Future<Long> subByAsync() throws Exception {
+        long start = System.currentTimeMillis();
+        long sum = 0;
+        Thread.sleep(DoTime);
+        long end = System.currentTimeMillis();
+        sum = end - start;
+        logger.info("\t 完成任务一");
+        return new AsyncResult<>(sum);
+    }
+
+    // 仅使用异步注解的方式实现异步方法
+    @Async
+    public void subByVoid() throws Exception {
+        long start = System.currentTimeMillis();
+        long sum = 0;
+        Thread.sleep(DoTime);
+        long end = System.currentTimeMillis();
+        sum = end - start;
+        logger.info("\t 完成任务二   ");
+        logger.info("注解任务执行的时间是： " + sum + "（毫秒）");
+    }
+
+    // 使用同步计算的方式--同步调用
+    public long subBySync() throws Exception {
+        long start = System.currentTimeMillis();
+        long sum = 0;
+        Thread.sleep(DoTime);
+        long end = System.currentTimeMillis();
+        sum = end - start;
+        logger.info("\t 完成任务三   ");
+        return sum;
+    }
+
 }
